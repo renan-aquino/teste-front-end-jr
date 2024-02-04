bb28133d8e3047526e6a27f6b26659349a167da5
Renan Aquino <renan.aquino99@gmail.com>
2024-02-04 06:28:51 -0300

    fix: correção de dados exibidos no card

--- src/components/product-card/product-card.tsx
+++ src/components/product-card/product-card.tsx
@@ -15,6 +15,7 @@ export function ProductCard(props : ProductProps){
 
     const price = formatPrice(props.price)
     const previous_price = formatPrice(props.price * 0.94)
+    const bill = formatPrice(props.price / 2)
 
     return (
         <div className={s.card}>
@@ -23,11 +24,11 @@ export function ProductCard(props : ProductProps){
                 <div className={s.image_box}>
                     <Image src={props.photo} alt={props.name} width={246} height={228}></Image>
                 </div>
-                <p className={s.name}>{'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}</p>
+                <p className={s.name}>{props.name}</p>
                 <div className={s.payment_info}>
                     <div className={s.previous_price}>{previous_price}</div>
                     <div className={s.price}>{price}</div>
-                    <div className={s.conditions}>ou 2x de {props.price / 2}</div>
+                    <div className={s.conditions}>ou 2x de {bill}</div>
                     <div className={s.shipping}>Frete grátis</div>
                 </div>
 
