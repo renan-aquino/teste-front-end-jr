bf21bc407850229b66657abe843928185c6d43b8
Renan Aquino <renan.aquino99@gmail.com>
2024-02-04 20:55:23 -0300

    feat: componentização dos títulos das seções

--- src/components/product-display/product-display.tsx
+++ src/components/product-display/product-display.tsx
@@ -2,15 +2,14 @@ import s from './product-display.module.scss'
 import { ProductSlider } from '../product-slider/product-slider';
 import { ProductList } from '../product-list/product-list';
 import { FilterByDevice } from '../filter-device/filter-by-device';
+import { SectionTitle } from '../section-title/section-title';
 
 
 export function ProductDisplay(){
     return (
         <section className={s.product_display}>
-            <div className={s.title_wrapper}>
-                <div className={s.line}/><h3>Produtos relacionados</h3><div className={s.line}/>
-            </div>
 
+            <SectionTitle text='Produtos relacionados' lines={true}/>
             <FilterByDevice/>
 
             <div className={s.product_container}>
@@ -18,6 +17,7 @@ export function ProductDisplay(){
                   <ProductList/>
                 </ProductSlider>
             </div>
+
         </section>
     )
 } 
---
+++ src/components/section-title/section-title.tsx
@@ -0,0 +1,17 @@
+import s from './section-title.module.scss'
+
+interface SectionTitleProps {
+    text: string,
+    lines: boolean
+}
+
+export function SectionTitle({text, lines} : SectionTitleProps) {
+
+    const display = lines ? 'block' : 'none'
+    
+    return(
+        <div className={s.title_wrapper}>
+            <div className={s.line} style={{display: display}} /><h3>Produtos relacionados</h3><div className={s.line} style={{display: display}}/>
+        </div>
+    )
+}
